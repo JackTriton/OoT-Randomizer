32f31c763927e256bba524e07d0d4697cc2fe5ba
JackTriton <35764777+JackTriton@users.noreply.github.com>
2025-07-29 10:16:04 +0900

    Update ASM ! Need to change asm_symbols.txt and c_symbols.txt as well !

--- ASM/src/messages.asm
+++ ASM/src/messages.asm
@@ -61,6 +61,55 @@ load_correct_message_segment:
     jr      $ra
     sw      t8, 0x0004(v0)
 
+Message_Decode_Control_Code_Hook_JP:
+    ;— replaced newline handling —
+    addiu   $at, r0, 0x000A
+    bne     v0, $at, @not_newline_jp
+    lh      t7, 0x008C($sp)
+    addiu   t8, t7, 0x0001
+    j       0x800DB4F0    ; return to JP decode loop on newline
+    sh      t8, 0x008C($sp)
+
+;— New JP Control Codes go here —
+
+@not_newline_jp:
+    addiu   sp, sp, -0x60
+    sw      a0, 0x10(sp)
+    sw      a1, 0x14(sp)
+    sw      a2, 0x18(sp)
+    sw      a3, 0x20(sp)
+
+    addiu   sp, sp, -0x20
+    sw      s5, 0x10(sp)
+    sw      s4, 0x14(sp)
+
+    lhu v0, 0xXXXX(sp)
+    or      a0, r0, v0
+    addiu   a1, sp, 0x10
+    addiu   a2, sp, 0x14
+
+    jal     Message_Decode_Additional_Control_Codes_JP
+    nop
+
+    lw      s5, 0x10(sp)
+    lw      s4, 0x14(sp)
+    addiu   sp, sp, 0x20
+
+    lw      a0, 0x10(sp)
+    lw      a1, 0x14(sp)
+    lw      a2, 0x18(sp)
+    lw      a3, 0x20(sp)
+
+    beqz    v0, @no_match_jp
+    lw      v0, 0x1C(sp)
+
+    j       0x800DB4F0
+    addiu   sp, sp, 0x60
+
+@no_match_jp
+    j       0x800DB32C
+    addiu   sp, sp, 0x60
+
 Message_Decode_Control_Code_Hook:
 ; Replaced code
     addiu   at, r0, 0x0001
